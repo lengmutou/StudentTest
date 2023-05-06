73e21aec57000c3e60a6758a9a7547d0c9b3370f
lengmu <1633808397@qq.com>
2023-05-06 08:42:26 +0800

    test update

---
+++ Test/src/jk.java
@@ -0,0 +1,8 @@
+/*
+ * @author  lengmu
+ * @version 1.0
+ */
+@FunctionalInterface
+public interface jk {
+ void test(String str);
+}
---
+++ Test/src/test.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.IntSummaryStatistics;
+import java.util.function.IntConsumer;
+
+/*
+ * @author  lengmu
+ * @version 1.0
+ */
+public class test extends Object {
+    public static void main(String[] args) {
+        int[] arr = {5,3,6,7,8,1};
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = 0; j <arr.length-i-1; j++) {
+                if (arr[j]>arr[j+1]){
+                    int a = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = a;
+                }
+            }
+        }
+//        Arrays.stream(arr).forEach(System.out::print);
+        test(System.out::println);
+    }
+    public static void test(jk jk){
+        jk.test("我是test");
+    }
+
+}
+abstract class test01{
+   test01 (){
+
+    }
+
+}
